9c5c254e534d3f04f9c734e4b5fb6392c253add9
radoslaw505 <radoslaw.chilimoniuk@gmail.com>
2020-06-01 23:37:19 +0200

    first commit

---
+++ src/main/java/com/binge/radoslaw/OracleJDBC/OracleJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.binge.radoslaw.OracleJDBC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OracleJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OracleJdbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/binge/radoslaw/OracleJDBC/OracleJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binge.radoslaw.OracleJDBC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OracleJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
