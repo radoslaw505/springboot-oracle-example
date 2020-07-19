8d832972d99132895a8faa65b96fb637ab31d50f
radoslaw505 <radoslaw.chilimoniuk@gmail.com>
2020-07-19 22:41:12 +0200

    commit

--- src/main/java/com/binge/radoslaw/OracleJDBC/config/SecurityConfig.java
+++ src/main/java/com/binge/radoslaw/OracleJDBC/config/SecurityConfig.java
@@ -1,23 +1,25 @@
-//package com.binge.radoslaw.OracleJDBC.config;
-//
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-//@EnableWebSecurity
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//    @Override
-//    protected void configure(HttpSecurity httpSecurity) throws Exception {
-//        httpSecurity
-//                .authorizeRequests()
-//                .antMatchers( "/login*", "/js/**", "/css/**").permitAll()
-//                .anyRequest().authenticated()
-//                .and()
-//                .formLogin()
-//                .loginPage("/login").permitAll()
-//                .defaultSuccessUrl("/operators", true)
-//                .failureForwardUrl("/login?error");
-//    }
-//
-//}
+package com.binge.radoslaw.OracleJDBC.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeRequests()
+                .antMatchers( "/login*", "/js/**", "/css/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login").permitAll()
+                .defaultSuccessUrl("/operators", true)
+                .failureForwardUrl("/login?error");
+    }
+
+}
